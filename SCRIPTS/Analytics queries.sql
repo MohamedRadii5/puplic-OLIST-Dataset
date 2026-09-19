958c8f41fc55ca6ffdda2e254ece3b5ef4d96403
Mohamed Radi Ahmed <m7mdelmoo@gmail.com>
2026-09-19 14:04:45 +0300

    Update Analytics queries.sql

--- SCRIPTS/Analytics queries.sql
+++ SCRIPTS/Analytics queries.sql
@@ -1,2 +1,16 @@
  -- EDA & Analytics
  --=========================================================
+
+-- [1] Top Cities Sales. 
+SELECT
+	ROW_NUMBER() OVER(ORDER BY SUM(price) DESC) AS row_num,
+	g.geolocation_city AS city,
+	SUM(price) AS sales
+FROM geolocation g
+JOIN customers c
+	ON g.geolocation_zip_code_prefix = c.customer_zip_code_prefix
+JOIN orders	 o
+	ON c.customer_id = o.customer_id
+JOIN order_items i
+	ON o.order_id = i.order_id
+GROUP BY geolocation_city;   
