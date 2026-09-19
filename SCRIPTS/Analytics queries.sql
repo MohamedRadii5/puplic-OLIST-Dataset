c73c3de6b61903e498f7e10ada828c226105fdcb
Mohamed Radi Ahmed <m7mdelmoo@gmail.com>
2026-09-19 14:05:27 +0300

    Update Analytics queries.sql

--- SCRIPTS/Analytics queries.sql
+++ SCRIPTS/Analytics queries.sql
@@ -14,3 +14,33 @@ JOIN orders	 o
 JOIN order_items i
 	ON o.order_id = i.order_id
 GROUP BY geolocation_city;   
+----------------------------------------------------------------------
+
+-- [2] Top Products Sales.
+
+-- 1. In brazilian language
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY COUNT(o.order_id) DESC) AS row_num,
+	p.product_category_name AS product_category,
+	COUNT(o.order_id) AS sales_count
+FROM products p
+JOIN order_items i
+	ON p.product_id = i.product_id
+JOIN orders o
+	ON i.order_id = o.order_id
+GROUP BY product_category_name;
+
+-- 2. In English language
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY COUNT(o.order_id) DESC) AS row_num,
+	t.product_category_name_english AS product_category,
+	COUNT(o.order_id) AS sales_count
+FROM product_category_name_translation t
+JOIN products p
+	ON t.product_category_name = p.product_category_name
+JOIN order_items i
+	ON p.product_id = i.product_id
+JOIN orders o
+	ON i.order_id = o.order_id
+GROUP BY product_category_name_english;
+----------------------------------------------------------------------
